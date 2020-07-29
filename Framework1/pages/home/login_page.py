f30c685253da7e6403d843231245a19e05d139b9
vaitheeshwaran.v <vaitheeshhh@gmail.com>
2020-07-29 21:47:42 +0530

    Modified overall structure with comment get element, etc

--- Framework1/pages/home/login_page.py
+++ Framework1/pages/home/login_page.py
@@ -1,8 +1,10 @@
 from selenium.webdriver.common.by import By
+from base.selenium_driver import SeleniumDriver
 
-class LoginPage():
+class LoginPage(SeleniumDriver):
 
     def __init__(self, driver):
+        super().__init__(driver)
         self.driver = driver
 
     # Locators
@@ -11,29 +13,29 @@ class LoginPage():
     _password_field = "user_password"
     _login_button = "commit"
 
-    def getLoginLink(self):
-        return self.driver.find_element(By.LINK_TEXT, self._login_link)
-
-    def getEmailField(self):
-        return self.driver.find_element(By.ID, self._email_field)
-
-    def getPasswordField(self):
-        return self.driver.find_element(By.ID, self._password_field)
-
-    def getLoginButton(self):
-        return self.driver.find_element(By.NAME, self._login_button)
+    # def getLoginLink(self):
+    #     return self.driver.find_element(By.LINK_TEXT, self._login_link)
+    #
+    # def getEmailField(self):
+    #     return self.driver.find_element(By.ID, self._email_field)
+    #
+    # def getPasswordField(self):
+    #     return self.driver.find_element(By.ID, self._password_field)
+    #
+    # def getLoginButton(self):
+    #     return self.driver.find_element(By.NAME, self._login_button)
 
     def clickLoginLink(self):
-        self.getLoginLink().click()
+        self.elementClick(self._login_link, locatorType="link")
 
     def enterEmail(self, email):
-        self.getEmailField().send_keys(email)
+        self.sendKeys(email, self._email_field)
 
     def enterPassword(self, password):
-        self.getPasswordField().send_keys(password)
+        self.sendKeys(password, self._password_field)
 
     def clickLoginButton(self):
-        self.getLoginButton().click()
+        self.elementClick(self._login_button, locatorType="name")
 
     def login(self, email, password):
         self.clickLoginLink()
