febdd9a506192bb276d64f3e314077c29bd638ba
vaitheeshwaran.v <vaitheeshhh@gmail.com>
2020-07-29 21:39:20 +0530

    Modified effectively in login page

--- Framework1/pages/home/login_page.py
+++ Framework1/pages/home/login_page.py
@@ -5,15 +5,38 @@ class LoginPage():
     def __init__(self, driver):
         self.driver = driver
 
-    def login(self, username, password):
-        loginLink = self.driver.find_element(By.LINK_TEXT, "Login")
-        loginLink.click()
+    # Locators
+    _login_link = "Login"
+    _email_field = "user_email"
+    _password_field = "user_password"
+    _login_button = "commit"
 
-        emailField = self.driver.find_element(By.ID, "user_email")
-        emailField.send_keys(username)
+    def getLoginLink(self):
+        return self.driver.find_element(By.LINK_TEXT, self._login_link)
 
-        passwordField = self.driver.find_element(By.ID, "user_password")
-        passwordField.send_keys(password)
+    def getEmailField(self):
+        return self.driver.find_element(By.ID, self._email_field)
 
-        loginButton = self.driver.find_element(By.NAME, "commit")
-        loginButton.click()
+    def getPasswordField(self):
+        return self.driver.find_element(By.ID, self._password_field)
+
+    def getLoginButton(self):
+        return self.driver.find_element(By.NAME, self._login_button)
+
+    def clickLoginLink(self):
+        self.getLoginLink().click()
+
+    def enterEmail(self, email):
+        self.getEmailField().send_keys(email)
+
+    def enterPassword(self, password):
+        self.getPasswordField().send_keys(password)
+
+    def clickLoginButton(self):
+        self.getLoginButton().click()
+
+    def login(self, email, password):
+        self.clickLoginLink()
+        self.enterEmail(email)
+        self.enterPassword(password)
+        self.clickLoginButton()
